c7ff3404e8d5a41d6e4faa0ea9b0e81dd88b439d
manuel <manuel.fresneda@um.es>
2021-02-23 19:35:26 +0100

    Tercer commit

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main() {
     printf("¡Hola mundo y esa gran gente de GitHub!\n");
     printf("¡Probando, si si, holaaaaaa!");
+    printf("¡Este si es el tercero de verdad, he cerrado el IDE y pruebo que no hay que configurar nada");
     return 0;
 }
