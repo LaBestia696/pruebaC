6f78724e4aab3946bd7c7144fac0c21b500ca9fc
manuel <manuel.fresneda@um.es>
2021-03-02 19:36:12 +0100

    Probando a día 2/3/2021

--- main.c
+++ main.c
@@ -4,5 +4,11 @@ int main() {
     printf("¡Hola mundo y esa gran gente de GitHub!\n");
     printf("¡Probando, si si, holaaaaaa!");
     printf("¡Este si es el tercero de verdad, he cerrado el IDE y pruebo que no hay que configurar nada");
+    printf("¡Aquí estamos de nueclear"
+           "vo, probando el Git a día 2/3/2021!");
     return 0;
 }
+
+
+
+
